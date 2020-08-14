8f9d7790815f3f79661aed510dfe4dfb7073da1e
rizwanishaq <rizwanishaq@gmail.com>
2020-08-14 09:27:41 +0200

    4th video completed

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,5 @@
+const User = require("../models/User");
+
 module.exports.signup_get = (req, res) => {
   res.render("signup");
 };
@@ -6,10 +8,19 @@ module.exports.login_get = (req, res) => {
   res.render("login");
 };
 
-module.exports.signup_post = (req, res) => {
-  res.send("new signup");
+module.exports.signup_post = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.create({ email, password });
+    res.status(201).json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send("erro, user not created");
+  }
 };
 
-module.exports.login_post = (req, res) => {
-  res.send("user login");
+module.exports.login_post = async (req, res) => {
+  const { email, password } = req.body;
+
+  res.send(req.body);
 };
