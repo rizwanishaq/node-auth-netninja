670899b18f3eb3a0ea4c51e85286640b32a0ba76
rizwanishaq <rizwanishaq@gmail.com>
2020-08-17 10:17:00 +0200

    hashing the password

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema(
   {
@@ -20,6 +21,19 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+// fire a function after doc saved to db
+userSchema.post("save", (doc, next) => {
+  console.log("new user was created and saved", doc);
+  next();
+});
+
+// fire a function before doc saved to db
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
 const user = mongoose.model("user", userSchema);
 
