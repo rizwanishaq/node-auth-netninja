0a017c3e94d099d3adceeebff5d0d25f7d458628
rizwanishaq <rizwanishaq@gmail.com>
2020-08-19 15:56:43 +0200

    cookies primer

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
 // middleware
 app.use(express.static("public"));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set("view engine", "ejs");
@@ -27,3 +29,22 @@ mongoose
 app.get("/", (req, res) => res.render("home"));
 app.get("/smoothies", (req, res) => res.render("smoothies"));
 app.use(authRoutes);
+
+// cookies
+app.get("/set-cookies", (req, res) => {
+  // res.setHeader("Set-Cookie", "newUser=true");
+  res.cookie("newUser", false);
+  res.cookie("isEployee", true, {
+    maxAge: 1000 * 60 * 60 * 24,
+    httpOnly: true,
+  });
+
+  res.send("you got the cookies");
+});
+
+app.get("/read_cookies", (req, res) => {
+  const cookies = req.cookies;
+  console.log(cookies);
+
+  res.json(cookies);
+});
