7cde9cb30b637c1a02543599c8d9e8ceea83ef78
rizwanishaq <rizwanishaq@gmail.com>
2020-08-24 11:58:10 +0200

    login

--- controllers/authController.js
+++ controllers/authController.js
@@ -52,5 +52,10 @@ module.exports.signup_post = async (req, res) => {
 module.exports.login_post = async (req, res) => {
   const { email, password } = req.body;
 
-  res.send(req.body);
+  try {
+    const user = await User.login(email, password);
+    res.status(200).json({ user: user._id });
+  } catch (err) {
+    res.status(400).json({});
+  }
 };
